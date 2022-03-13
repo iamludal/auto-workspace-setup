30cc27183a90157d6f30dabf1bbc03037e8b54a2
Ludal <ludovic.chombeau@gmail.com>
2022-03-13 17:02:50 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-dotfiles_dir=$PWD/$(dirname $0)
-excluded=(.git README.md install.sh)
+excluded=(.git README.md flameshot.conf)
 
 create_symlinks()
 {
-  find_cmd="find . -maxdepth 1"
+  dotfiles_dir=$(dirname `readlink -f $0`)
+  current_script=$(basename $0)
+  command="ls -A $dotfiles_dir"
+  excluded+=($current_script)
 
-  for file in ${EXCLUDED[@]}; do
-    find_cmd="$find_cmd -not -name $file"
+  for file in ${excluded[@]}; do
+    command="$command -I $file"
   done
 
-  hidden_files=$(eval $find_cmd)
+  hidden_files=$(eval $command)
 
-  for file in $hidden_files
-  do
+  echo $command
+  echo $hidden_files
+
+  exit -1
+
+  for file in $hidden_files; do
     ln -sf $dotfiles_dir/$file ~/$file
-    
+
     if [[ $? -eq 0 ]]
     then
         echo "[i] $file : ok"
@@ -26,15 +32,18 @@ create_symlinks()
   done
 }
 
-echo -n "[?] Create symbolic links? This will overwrite your exiting ones. [y/N] "
-read
+main()
+{
+  read -p "[?] Create symbolic links? This will overwrite your exiting ones. [y/N] " answer
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo "[i] Creating symlinks..."
-  create_symlinks
-else
-  echo "[x] Aborted"
-  exit 1
-fi
+  if [[ $answer =~ ^[Yy]$ ]]
+  then
+    echo "[i] Creating symlinks..."
+    create_symlinks
+  else
+    echo "[x] Aborted"
+    exit 1
+  fi
+}
 
+main
